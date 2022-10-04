64a4df93ca230404dce9d79774127e265202bd54
Renan <70537760+renan823@users.noreply.github.com>
2022-10-04 15:00:25 -0300

    Add files via upload

---
+++ novo_insercao.sql
@@ -0,0 +1,35 @@
+insert into transporte values(1, 'Avião', 400, 'Airbus 500', 'Gasolina A.', 200);
+insert into transporte values(2, 'Avião', 400, 'Airbus 500', 'Gasolina A.', 200);
+insert into transporte values(3, 'Avião', 400, 'Airbus 500', 'Gasolina A.', 200);
+insert into transporte values(4, 'Avião', 400, 'Airbus 500', 'Gasolina A.', 200);
+insert into transporte values(5, 'Avião', 400, 'Airbus 500', 'Gasolina A.', 200);
+
+
+insert into destino values('Singapura', 'Singapura', 'Singapura', 5686000, '-12812.75 -34567.90');
+insert into destino values('Manaus', 'Amazonas', 'Brasil', 2000000, '-12367.10 -23897.60');
+insert into destino values('Paris', 'Ile-de-France', 'França', 2148279, '-12345.00 -23451.10');
+insert into destino values('São Paulo', 'São Paulo', 'Brasil', 12000000, '-98213.10 -99866.22');
+insert into destino values('Tóquio', 'Tóquio', 'Japão', 14000000, '-78222.05 -96001.50');
+
+
+insert into pTuristicoDestino values('Manaus', 'Amazonas', 'Brasil', 'Teatro de Manaus');
+insert into pTuristicoDestino values('São Paulo', 'São Paulo', 'Brasil', 'MASP');
+insert into pTuristicoDestino values('São Paulo', 'São Paulo', 'Brasil', 'Instituto Butantã');
+insert into pTuristicoDestino values('Singapura', 'Singapura', 'Singapura', 'Marina Bay Sands');
+insert into pTuristicoDestino values('Paris', 'Ile-de-France', 'França', 'Torre Eiffel');
+
+
+
+insert into fusoDestino values('Paris', 'Ile-de-France', 'França', 0);
+insert into fusoDestino values('Singapura', 'Singapura', 'Singapura', 8);
+insert into fusoDestino values('Manaus', 'Amazonas', 'Brasil', -4);
+insert into fusoDestino values('São Paulo', 'São Paulo', 'Brasil', -3);
+insert into fusoDestino values('Tóquio', 'Tóquio', 'Japão', 9);
+
+
+
+insert into viagem values(1, 'Singapura', 'Singapura', 'Singapura', 1, 2000, 8, '2022/07/02', '2022/07/02', 3090);
+insert into viagem values(2,'Manaus', 'Amazonas', 'Brasil', 2, 500, 3, '2022/07/02', '2022/07/02', 3090);
+insert into viagem values(3,'Paris', 'Ile-de-France', 'França', 3, 1000, 5, '2022/07/02', '2022/07/02', 3090);
+insert into viagem values(4,'São Paulo', 'São Paulo', 'Brasil', 4, 500, 3, '2022/12/02', '2022/12/05', 1000);
+insert into viagem values(5,'Tóquio', 'Tóquio', 'Japão', 5, 5000, 10, '2022/06/02', '2022/06/08', 5500);
